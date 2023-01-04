5a8108acc43d335ac921410d6cef3789d1a0d36f
harini-spec <ponshriharini@gmail.com>
2023-01-04 19:40:00 +0530

    03. Added Movie Info API

--- movie-info-service/src/main/java/io/harini/controllers/InfoController.java
+++ movie-info-service/src/main/java/io/harini/controllers/InfoController.java
@@ -1,5 +1,19 @@
 package io.harini.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.harini.models.InfoItem;
+
+@RestController
+@RequestMapping("/movies")
 public class InfoController {
+	
+	@RequestMapping("{id}")
+	public InfoItem getInfo(@PathVariable("id") String movieId){
+		return new InfoItem(movieId,"Alice in Worderland");
+	}
 
 }
