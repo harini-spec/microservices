96673e5795de1fe69b5185c90f5c2e94cb9da268
harini-spec <ponshriharini@gmail.com>
2023-01-04 23:27:25 +0530

    07. Used WebClient to call the rest api of other services

--- movie-catalog-service/src/main/java/io/harini/controller/CatalogController.java
+++ movie-catalog-service/src/main/java/io/harini/controller/CatalogController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import io.harini.models.CatalogItem;
 import io.harini.models.InfoItem;
@@ -22,17 +23,23 @@ public class CatalogController {
 	@Autowired
 	private RestTemplate restTemplate;
 	
+	@Autowired
+	private WebClient.Builder webClientBuilder;
+	
 	@GetMapping("/{id}")
 	public List<CatalogItem> getDetails(@PathVariable("id") String userId){
 		
-		// get all movie ids
-		
 		List<Ratings> ratings = Arrays.asList(new Ratings("1234",4), new Ratings("5678",3));
 		
-		// get movie name and details for each movie id
-		
 		return ratings.stream().map(rating -> {
-			InfoItem movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieID(),InfoItem.class);
+			
+			InfoItem movie = webClientBuilder.build()
+					.get()
+					.uri("http://localhost:8082/movies/\"+rating.getMovieID()")
+					.retrieve()
+					.bodyToMono(InfoItem.class)
+					.block();
+			
 			return new CatalogItem(movie.getMovieName(), "Goood", rating.getRating());
 		})
 		.collect(Collectors.toList());                     // to convert the result after iteration into a list
